8ad0e40952c2cf5449aba40a2290bda124f84217
Riccardo Isola <riky.isola@gmail.com>
2024-12-23 13:07:24 +0100

    update contract

--- contract/contracts/SmartChallenge.sol
+++ contract/contracts/SmartChallenge.sol
@@ -7,29 +7,32 @@ pragma solidity ^0.8.0;
 
 contract SmartChallenge {
     struct Challenge {
-        uint challengeId;
-        string flagHash;
-        uint8 reward;
-        string hash;
+        address publicFlag;
+        uint reward;
+        uint score;
+    }
+
+    struct ChallengesSet{
+        mapping(uint=>bool) is_in;
+        uint[] values;
     }
 
     struct Player {
-        bool isRegistered;
-        uint256 score;
-        mapping(uint => bool) solvedChallenges;
-        string hash;
+        ChallengesSet solvedChallenges;
     }
 
-    mapping(uint => Challenge) public challenges;
-    mapping(address => Player) public players;
-    address[] public playerAddresses;
-    address payable owner;
-    uint8 public constant costFlagSend = 1;
+    struct PlayersMap{
+        mapping(address=>Player) values;
+        address[] keys;
+        mapping(address=>bool) is_in;
+    }
 
-    uint32 public challengeCounter;
+    Challenge[] public challenges;
+    PlayersMap players;
+    address payable owner;
 
     event ChallengeSubmitted(string returnValue);
-    event ChallengeAdded(uint256 indexed challengeId, string _flag, uint256 _reward, string _hash);
+    event ChallengeAdded(uint indexed challengeId, address _publicFlag, uint _reward, uint _score);
 
     constructor() payable {
         owner = payable(msg.sender);
@@ -44,80 +47,135 @@ contract SmartChallenge {
         return owner;
     }
 
-    function payUser(address payable recipient, uint8 amount) internal {
+    function payUser(address payable recipient, uint amount) internal {
         require(address(this).balance >= amount, "Insufficient contract balance");
         recipient.transfer(amount);
     }
 
-    function submitFlag(uint _challengeId, string memory _flag) public payable {
-        require(challenges[_challengeId].challengeId == _challengeId, "Challenge does not exist");
-        require(bytes(_flag).length > 0, "Flag cannot be empty");
-        require(!players[msg.sender].solvedChallenges[_challengeId], "Challenge already solved by this player");
-        require (msg.value >= costFlagSend, "Not enough wei to submit");
-        if(keccak256(abi.encodePacked(challenges[_challengeId].flagHash)) == keccak256(abi.encodePacked(_flag))){
+    function getMessageHash(
+        address _address,
+        uint _id
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_address,_id));
+    }
+
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        public
+        pure
+        returns (bytes32)
+    {
+        /*
+        Signature is produced by signing a keccak256 hash with the following format:
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        */
+        return keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+        );
+    }
+
+    function verify(
+        address _address,
+        uint _id,
+        bytes memory signature,
+        address _publicFlag
+    ) public pure returns (bool) {
+        bytes32 messageHash = getMessageHash(_address, _id);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+        return recoverSigner(ethSignedMessageHash, signature) == _publicFlag;
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (bytes32 r, bytes32 s, uint8 v)
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+
+    function submitFlag(uint _challengeId, bytes memory signature) public payable {
+        require(_challengeId>=challenges.length, "Challenge does not exist");
+        require(bytes(signature).length > 0, "Flag cannot be empty");
+        require(!players.values[msg.sender].solvedChallenges.is_in[_challengeId], "Challenge already solved by this player");
+
+        bool verified=verify(msg.sender,_challengeId, signature, challenges[_challengeId].publicFlag);
+
+
+        if(verified){
             emit ChallengeSubmitted("Correct answer!");
-            players[msg.sender].solvedChallenges[_challengeId] = true;
-            players[msg.sender].score += challenges[_challengeId].reward;
+            players.values[msg.sender].solvedChallenges.is_in[_challengeId] = true;
+            players.values[msg.sender].solvedChallenges.values.push(_challengeId);
 
             payUser(payable (msg.sender), challenges[_challengeId].reward);
 
-            if (!players[msg.sender].isRegistered) {
-                players[msg.sender].isRegistered = true;
-                playerAddresses.push(msg.sender);
+            if (!players.is_in[msg.sender]) {
+                players.is_in[msg.sender]=true;
+                players.keys.push(msg.sender);
             }
         } else{
             emit ChallengeSubmitted("Incorrect Flag!");
         }
     }
 
-    function addChallenge(string memory _flag, uint8 _reward, string memory _hash) public onlyOwner {
-        require(bytes(_flag).length > 0, "Flag cannot be empty");
-        challenges[challengeCounter] = Challenge(challengeCounter, _flag, _reward,  _hash);
-        challengeCounter++;
-        emit ChallengeAdded(challengeCounter, _flag, _reward, _hash);
-    }
-
-    function updatePlayer(string memory _hash) public{
-        players[msg.sender].hash = _hash;
-    }
-
-    function getPlayer() public view returns (string memory) {
-        Player storage player = players[msg.sender];
-        return (player.hash);
+    function addChallenge(address _flag, uint8 _reward, uint8 _score) public onlyOwner {
+        challenges.push(Challenge(_flag, _reward,  _score));
+        emit ChallengeAdded(challenges.length-1, _flag, _reward, _score);
     }
 
     function getChallenges() public view returns (Challenge[] memory) {
-        Challenge[] memory allChallenges = new Challenge[](challengeCounter);
-
-        for (uint i = 0; i < challengeCounter; i++) {
-            allChallenges[i] = challenges[i];
-        }
-        return allChallenges;
+        return challenges;
     }
 
     function getScore(address _player) public view returns (uint) {
-        return players[_player].score;
+        uint result=0;
+        for(uint i=0;i<players.values[_player].solvedChallenges.values.length;i++){
+            result+=challenges[players.values[_player].solvedChallenges.values[i]].score;
+        }
+        return result;
     }
 
     function getPlayers() public view returns (address[] memory) {
-        return playerAddresses;
+        return players.keys;
     }
 
-    function IsChallengeSolved(address playerAddress, uint challengeIndex) public view returns (bool) {
-        return players[playerAddress].solvedChallenges[challengeIndex];
+    function isChallengeSolved(address playerAddress, uint challengeIndex) public view returns (bool) {
+        return players.values[playerAddress].solvedChallenges.is_in[challengeIndex];
     }
 
     function getScores() public view returns (address[] memory, uint[] memory) {
-        uint[] memory playerScores = new uint[](playerAddresses.length);
-        for (uint i = 0; i < playerAddresses.length; i++) {
-            playerScores[i] = players[playerAddresses[i]].score;
+        uint[] memory playerScores = new uint[](players.keys.length);
+        for (uint i = 0; i < players.keys.length; i++) {
+            playerScores[i]=getScore(players.keys[i]);
         }
-        return (playerAddresses, playerScores);
-    }
-
-    function withdraw() public payable onlyOwner {
-        uint256 balance = address(this).balance;
-        require (balance > 0, "The balance is zero, nothing to transfer");
-        owner.transfer(balance);
+        return (players.keys, playerScores);
     }
 }
